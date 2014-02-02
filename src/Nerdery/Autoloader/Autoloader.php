0803419a45b8574b555d2b7dbfb5e0dabaa3379a
Douglas Linsmeyer <dlinsmey@nerdery.com>
2014-02-01 20:54:27 -0800

    Adds composer.lock to .gitignore

---
+++ src/Nerdery/Autoloader/Autoloader.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * File Autoloader.php
+ * 
+ * @author Douglas Linsmeyer <douglas.linsmeyer@nerdery.com>
+ */
+
+namespace Nerdery\Autoloader;
+
+
+/**
+ * Class Autoloader
+ *
+ * @package Nerdery\Autoloader
+ * @author Douglas Linsmeyer <douglas.linsmeyer@nerdery.com>
+ */
+class Autoloader 
+{
+
+} 
